cb85ac8079d9415acc044c1696840639b36594a6
ozen-hub <info.ozenhub@gmail.com>
2023-09-12 21:16:10 +0530

    2

--- src/main/java/com/app/servlets/CustomerServlet.java
+++ src/main/java/com/app/servlets/CustomerServlet.java
@@ -1,5 +1,8 @@
 package com.app.servlets;
 
+import com.app.controller.CustomerController;
+import com.app.entity.Customer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +13,22 @@ import java.io.IOException;
 @WebServlet("/save")
 public class CustomerServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Done!");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id=req.getParameter("id");
+        String name=req.getParameter("name");
+        String address=req.getParameter("address");
+        String salary=req.getParameter("salary");
+
+        Customer customer = new Customer(
+                Long.parseLong(id),name,address,Double.parseDouble(salary)
+        );
+        System.out.println(customer);
+        if (new CustomerController().saveCustomer(customer)){
+            System.out.println("saved!");
+            resp.sendRedirect("new_customer.jsp");
+        }else{
+            System.out.println("error");
+        }
+
     }
 }
